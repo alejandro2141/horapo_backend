38be6bb6720aa005a8b6b8bcbbb6a5ffda7766c9
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2021-02-17 18:33:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/hola', (req, res) => {
   res.send('Hola hola pirinola ');
 });
 
+// App
+const app = express();
+app.get('/chao', (req, res) => {
+  res.send('chaolin bombin ');
+});
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
