bc5c98b0a4178d6ce6a3ae3ec093c45dc6bedf35
alejandro2141 <alejandro2141@gmail.com>
2021-02-25 10:41:07 -0300

    Rest Get Appoitment v1

--- server.js
+++ server.js
@@ -10,157 +10,91 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
+// CONN POSTGRESQL
+/*
+const { Pool, Client } = require('pg')
+const client = new Client({
+  user: 'conmeddb_user',
+  host: '127.0.0.1',
+  database: 'conmeddb01',
+  password: 'paranoid',
+  port: 5432,
+})
+*/
+//and connect to Postges
+//client.connect()
+//*************
+
+
+// APP SET CORS to allow Al Origins
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let usuario = {
- nombre:'aaa',
- apellido: 'bbb'
-};
-let respuesta = {
- error: false,
- codigo: 200,
- mensaje: ''
-};
-
 
 //APP Functions
-/*
-app.get('/hola', (req, res) => {
-  res.send('hoy hola GET ');
-});
+
+  
+//********************************************* 
+// PUBLIC GET APPOINTMENT BKP
+//********************************************* 
+ /*
+app.route('/public_appointment')
+.get(function (req, res) {
+ 
+client.connect()
+
+const resultado = client.query('SELECT * FROM appointment', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
+  
+ res.send("saludos terricolas");
+  //res.status(200).json(res.rows)
+  // res.send(res);
+   
+ })
 */
 
-app.route('/public_appointments')
- .get(function (req, res) {
-  
-  respuesta = {
-  
-	   "code": 200,
-	   "mainDate": "SEPT 29",
-	   "app_available" : {
-	   
-	   	"app_id" : "000001",
-	   	"date" : "28-08-2021",
-	   	"time" : "08:30" ,
-	   	"doc_id" : "000001",
-	   	"doc_name" : "Dr Juan Carlos Gonzales",
-	   
-	   }
-  
-  };
-  
-  /*
-  
-  if(usuario.nombre === '' || usuario.apellido === '') {
-   respuesta = {
-    error: true,
-    codigo: 501,
-    mensaje: 'El usuario no ha sido creado'
-   };
-  } else {
-   respuesta = {
-    error: false,
-    codigo: 200,
-    mensaje: 'respuesta del usuario',
-    respuesta: usuario
-   };
-  }
-  */
-  res.send(respuesta);
- })
+//********************************************* 
+// PUBLIC GET APPOINTMENT BKP
+//********************************************* 
+ 
+app.route('/public_appointment')
+.get(function (req, res) {
  
  
+const { Pool, Client } = require('pg')
+const client = new Client({
+  user: 'conmeddb_user',
+  host: '127.0.0.1',
+  database: 'conmeddb01',
+  password: 'paranoid',
+  port: 5432,
+})
+client.connect()
+
+const resultado = client.query('SELECT * FROM appointment', (err, result) => {
+  console.log(err, JSON.stringify(result))
+  res.status(200).send(JSON.stringify(result))
+  client.end()
+})
+  
+ //res.send("saludos terricolas");
+  //res.status(200).json(resultado.rows) ;
+  // res.send(JSON.stringify(result));
+})
  
- .post(function (req, res) {
-  if(!req.body.nombre || !req.body.apellido) {
-   respuesta = {
-    error: true,
-    codigo: 502,
-    mensaje: 'El campo nombre y apellido son requeridos'
-   };
-  } else {
-   if(usuario.nombre !== '' || usuario.apellido !== '') {
-    respuesta = {
-     error: true,
-     codigo: 503,
-     mensaje: 'El usuario ya fue creado previamente'
-    };
-   } else {
-    usuario = {
-     nombre: req.body.nombre,
-     apellido: req.body.apellido
-    };
-    respuesta = {
-     error: false,
-     codigo: 200,
-     mensaje: 'Usuario creado',
-     respuesta: usuario
-    };
-   }
-  }
-  
-  res.send(respuesta);
- })
- .put(function (req, res) {
-  if(!req.body.nombre || !req.body.apellido) {
-   respuesta = {
-    error: true,
-    codigo: 502,
-    mensaje: 'El campo nombre y apellido son requeridos'
-   };
-  } else {
-   if(usuario.nombre === '' || usuario.apellido === '') {
-    respuesta = {
-     error: true,
-     codigo: 501,
-     mensaje: 'El usuario no ha sido creado'
-    };
-   } else {
-    usuario = {
-     nombre: req.body.nombre,
-     apellido: req.body.apellido
-    };
-    respuesta = {
-     error: false,
-     codigo: 200,
-     mensaje: 'Usuario actualizado',
-     respuesta: usuario
-    };
-   }
-  }
-  
-  res.send(respuesta);
- })
- .delete(function (req, res) {
-  if(usuario.nombre === '' || usuario.apellido === '') {
-   respuesta = {
-    error: true,
-    codigo: 501,
-    mensaje: 'El usuario no ha sido creado'
-   };
-  } else {
-   respuesta = {
-    error: false,
-    codigo: 200,
-    mensaje: 'Usuario eliminado'
-   };
-   usuario = { 
-    nombre: '', 
-    apellido: '' 
-   };
-  }
-  res.send(respuesta);
- });
-app.use(function(req, res, next) {
- respuesta = {
-  error: true, 
-  codigo: 404, 
-  mensaje: 'URL no encontrada'
- };
- res.status(404).send(respuesta);
-});
+
+
 
 
 
