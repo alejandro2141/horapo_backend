9764010d19d5dec617834f793362d830625fae82
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-01-24 19:16:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2453,7 +2453,6 @@ async function professional_get_data_for_calendars_view(json)
     }
 
     return json_response
-
 }
 
 //******************************************************** */
@@ -2972,14 +2971,50 @@ async function professional_get_appointments_from_calendars(prof_id, date,remove
 /******************************************************************** */
 /******************************************************************** */
 
+// ********** 24-01-2023 
+app.route('/public_cancel_app')
+.post(function (req, res) {
+    console.log('public_cancel_app INPUT : ', req.body );
+// ****** Connect to postgre
+const { Client } = require('pg')
+const client = new Client(conn_data)
+client.connect()
+
+/* INPUT
+    req_date : req_date,
+    req_id  : req_id,
+    req_center_id :  req_center_id,
+    req_patient_doc_id : req_patient_doc_id,
+ */
+
+//let query_reserve =   "INSERT INTO appointment (  date , start_time,  duration,  center_id, confirmation_status, professional_id, patient_doc_id, patient_name,    patient_email, patient_phone1,  patient_age,  app_available, app_status, app_blocked, app_public,  location1, location2, location3, location4, location5, location6,   app_type_home, app_type_center,  app_type_remote, patient_notification_email_reserved , specialty_reserved , patient_address , calendar_id )"   
+let query_reserve = "DELETE FROM appointment WHERE id="+req.body.req_id+" AND center_id="+req.body.req_center_id+" AND patient_doc_id='"+req.body.req_patient_doc_id+"' "
+
+//query_reserve  += " VALUES ( '"+req.body.appointment_date+"' , '"+req.body.appointment_start_time+"' , '"+req.body.appointment_duration+"' ,  "+req.body.appointment_center_id+" , '0' , '"+req.body.appointment_professional_id+"' , '"+req.body.patient_doc_id.toUpperCase() +"' , '"+req.body.patient_name.toUpperCase()+"' , '"+req.body.patient_email.toUpperCase()+"' , '"+req.body.patient_phone+"' ,  '"+req.body.patient_age+"' ,'false' , '1' , '0' , '1', "+req.body.appointment_location1+" , "+req.body.appointment_location2+" ,"+req.body.appointment_location3+" ,"+req.body.appointment_location4+" ,"+req.body.appointment_location5+" ,"+req.body.appointment_location6+" , '"+req.body.appointment_type_home+"' , '"+req.body.appointment_type_center+"' , '"+req.body.appointment_type_remote+"' , '1' , '"+req.body.appointment_specialty+"' , '"+req.body.patient_address+"'  , '"+req.body.appointment_calendar_id+"' 	) RETURNING * " ; 
+
+console.log(query_reserve);
+const resultado = client.query(query_reserve, (err, result) => {
+    //res.status(200).send(JSON.stringify(result)) ;
+    if (err) {
+      console.log('/public_cancel_app ERR:'+err ) ;
+    }
+    else {
+    console.log("public_cancel_app JSON RESPONSE BODY : "+JSON.stringify(result));
+    res.status(200).send(JSON.stringify(result.rows[0])) ;  
+    }
+    client.end()
+})
+
+})
+
+
+
 // PROFESSIONAL GET SPECIALT 
 app.route('/get_professional_specialty')
 .post(function (req, res) {
  
     console.log('get_professional_specialty:', req.body );
  
-// ****** Connect to postgre
-// ****** Connect to postgre
 const { Client } = require('pg')
 const client = new Client(conn_data)
 client.connect()
@@ -3425,7 +3460,6 @@ async function get_all_professional_calendars(prof_id)
   return res.rows;
 }
 
-
 //GET PROFESSIONAL Centers
 async function get_professional_centers(id)
 {
@@ -3490,25 +3524,6 @@ async function get_calendars_available_by_professional_date(prof_id,date)
   client.end() 
   return res.rows ;
 }
-
-/*
-//GET CALENDARS BY Professional ID
-async function get_calendars_available_by_ProfessionalId(prof_id,date)
-{
-  const { Client } = require('pg')
-  const client = new Client(conn_data)
-  await client.connect()  
-  //END IF LOCATION
-  //const sql_calendars  = "SELECT * FROM professional_calendar WHERE id = 139 AND date_start <='2022-06-02' AND date_end >= '2022-06-01' AND  active = true AND deleted_professional = false AND status = 1  " ;  
-  const sql_calendars  = "SELECT * FROM professional_calendar WHERE professional_id = "+prof_id+" AND  active = true AND deleted_professional = false AND status = 1  AND date_start <= '"+date+"'  AND date_end >= '"+date+"'  " ;  
-
-  console.log("get_calendar_available_by_ProfessionalId  SQL:"+sql_calendars) 
-  
-  const res = await client.query(sql_calendars) 
-  client.end() 
-  return res.rows ;
-}
-*/
 
 //GET ALL ACTIVE CALENDARS BY Professional ID
 async function get_all_calendars_available_by_professional_id(prof_id)
