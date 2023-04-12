5a7763dea3ccfbaa98e41caed5a61642156cacb2
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-04-12 16:06:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4216,7 +4216,8 @@ function sntz_json(json_input,service_name)
   const keys = Object.keys(json_input);
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i]; 
-    if (typeof json_input[key] === 'string')
+    
+    if (typeof json_input[key] === 'string' || myVar instanceof String )
     {
       json_input[key]=sntz(json_input[key]) 
       
