25ca6a7d6d5bded97bcf594ce108a7df26c491a5
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-09-07 18:22:58 -0300

    Update LoadTest.sql

--- dbdump/LoadTest.sql
+++ dbdump/LoadTest.sql
@@ -23,13 +23,30 @@ v_profname text;
 v_profid bigint;
 v_centerid1 bigint;
 v_centerid2 bigint;
+v_centerid3 bigint;
 v_calendarid1 bigint;
 v_calendarid2 bigint;
+v_calendarid3 bigint;
 
-v_calendarDateStart1 text;
-v_calendarDateEnd1 text;
-v_calendarTimeStart1 text;
-v_calendarTimeEnd1 text;
+v_calendarDateStart1 date ;
+v_calendarDateEnd1 date ;
+v_calendarTimeStart1 time ;
+v_calendarTimeEnd1 time ;
+
+v_calendarDateStart2 date ;
+v_calendarDateEnd2 date ;
+v_calendarTimeStart2 time ;
+v_calendarTimeEnd2 time ;
+
+v_calendarDateStart3 date ;
+v_calendarDateEnd3 date ;
+v_calendarTimeStart3 time ;
+v_calendarTimeEnd3 time ;
+
+v_specialty bigint;
+
+cnt bigint;
+cnt_specialty bigint;
 
 
 BEGIN 
@@ -37,7 +54,19 @@ BEGIN
 v_calendarDateStart1 	= '2023-08-23T04:00:00.000Z' ;
 v_calendarDateEnd1 		= '2023-09-23T04:00:00.000Z'  ;
 v_calendarTimeStart1 	= '09:00:00-4' ;
-v_calendarTimeEnd1 		= '10:00:00-4' ;
+v_calendarTimeEnd1 		= '14:00:00-4' ;
+
+v_calendarDateStart2 	= '2023-08-23T04:00:00.000Z' ;
+v_calendarDateEnd2 		= '2023-09-23T04:00:00.000Z'  ;
+v_calendarTimeStart2 	= '16:00:00-4' ;
+v_calendarTimeEnd2 		= '19:00:00-4' ;
+
+v_calendarDateStart3 	= '2023-09-15T04:00:00.000Z' ;
+v_calendarDateEnd3 		= '2023-10-23T04:00:00.000Z'  ;
+v_calendarTimeStart3 	= '20:00:00-4' ;
+v_calendarTimeEnd3 		= '23:00:00-4' ;
+
+
 
 
 TRUNCATE TABLE professional_calendar;
@@ -49,10 +78,19 @@ TRUNCATE TABLE appointment  CASCADE;
 
 
 
--- CREATE PROFESSIONALS BASED IN COUNTER
-	for cnt in 1..2 loop 
 
-		v_profname := CONCAT('prof',cnt);
+-----------------------
+--   CREATE KINE ------
+-----------------------
+
+
+for cnt_specialty in 1..8 loop 
+
+v_specialty = 100 * cnt_specialty;
+
+	for cnt in 1..8 loop 
+
+		v_profname := CONCAT('profKine',cnt);
 		-- INSERT PROFESSIONAL DATA
 		INSERT INTO professional (name, document_number, license_number,  email, address, phone , active) VALUES (v_profname, CONCAT(CONCAT( CONCAT(CONCAT(cnt,cnt)),'-'), cnt )  ,'123', CONCAT(v_profname,'@nada.com'), 'avsiempreviva', '77777', true   ) 
 		RETURNING id INTO v_profid;
@@ -60,51 +98,70 @@ TRUNCATE TABLE appointment  CASCADE;
 		INSERT INTO account  (user_id, pass , active ) VALUES ( v_profid , cnt , true ) ;
 		
 		--INSERT TWO SPECIALTIES to this professional
-		INSERT INTO professional_specialty ( professional_id , specialty_id ) VALUES ( v_profid , '100' ) ;
-		INSERT INTO professional_specialty ( professional_id , specialty_id ) VALUES ( v_profid , '200' ) ;
+		INSERT INTO professional_specialty ( professional_id , specialty_id ) VALUES ( v_profid , v_specialty) ;
 		
 		-- INSERT CENTERs
 		INSERT INTO center ( name , address , phone1,phone2, active, country, comuna, center_deleted, center_color, home_comuna1, home_comuna2 , home_visit,center_visit, remote_care, professional_id , date  ) 
-	                values ( CONCAT('consulta1',v_profname) , CONCAT('Avenida consulta1 ', v_profname) ,  '7454333' , '56975397201', 1 , 1 , 1511 , false , '#FFE6EE' , 1708 , 1662,  false  , true , false ,  v_profid ,  NOW() )  
+	                values ( CONCAT('consulta1',v_profname) , CONCAT('Avenida consulta1 ', v_profname) ,  '7454333' , '56975397201', 1 , 1 , 1511 , false , '#FFE6EE' , null , null ,  false  , true , false ,  v_profid ,  NOW() )  
 					RETURNING id INTO v_centerid1;
 					
 		INSERT INTO center ( name , address , phone1,phone2, active, country, comuna, center_deleted, center_color, home_comuna1, home_comuna2 , home_visit,center_visit, remote_care, professional_id , date  ) 
-	                values ( CONCAT('consulta2',v_profname) , CONCAT('Avenida consulta2 ', v_profname) ,  '7454333' , '56975397201', 1 , 1 , 1512 , false , '#FF4244' , 1709 , 1662,  false  , true , false ,  v_profid ,  NOW() )  
+	                values ( CONCAT('consulta2',v_profname) , null ,  '7454333' , '56975397201', 1 , 1 , null , false , '#FF4244' , 1511, 1509,  true  , false , false ,  v_profid ,  NOW() )  
 					RETURNING id INTO v_centerid2; 
+	
+		INSERT INTO center ( name , address , phone1,phone2, active, country, comuna, center_deleted, center_color, home_comuna1, home_comuna2 , home_visit,center_visit, remote_care, professional_id , date  ) 
+	                values ( CONCAT('consulta2',v_profname) , null  ,  '7454333' , '56975397201', 1 , 1 , null , false , '#FF4244' , null , null ,  false  , false , true ,  v_profid ,  NOW() )  
+					RETURNING id INTO v_centerid3; 
 					
 						
 		-- INSERT CALENDARs
 		--calendarColorArray : ["#FF4244","#4ebeef","#AF8536", "#f6a700", "#32b780", "#dd6da4"],
 		INSERT INTO professional_calendar (professional_id , start_time,  end_time, specialty1, duration, time_between, monday, tuesday, wednesday, thursday, friday, saturday , sunday, date_start, date_end,   center_id,  status , deleted_professional, color ,date ,price , active ) 
-		VALUES ( v_profid,  '13:00:00-4' , '14:00:00-4' , '100' , '50' , '10' ,  'true' ,  'true'  ,  'true' ,  'true'  ,  'true'  , 'true'  ,  'false'  , '2023-08-23T04:00:00.000Z',  '2023-09-05T04:00:00.000Z', v_centerid1 , '1' , false , '#FF4244' ,  NOW() , 666 , true ) 
+		VALUES ( v_profid,  v_calendarTimeStart1 , v_calendarTimeEnd1  , v_specialty , '50' , '10' ,  'true' ,  'true'  ,  'true' ,  'true'  ,  'true'  , 'true'  ,  'false'  , v_calendarDateStart1 ,  v_calendarDateEnd2 , v_centerid1 , '1' , false , '#FF4244' ,  NOW() , 66666 , true ) 
 		RETURNING id INTO v_calendarid1 ; 
 		
 		INSERT INTO professional_calendar (professional_id , start_time,  end_time, specialty1, duration, time_between, monday, tuesday, wednesday, thursday, friday, saturday , sunday, date_start, date_end,   center_id,  status , deleted_professional, color ,date ,price , active ) 
-		VALUES ( v_profid,  '08:00:00-4' , '11:00:00-4' , '100' , '45' , '15' ,  'true' ,  'true'  ,  'true' ,  'true'  ,  'true'  , 'false'  ,  'false'  , '2023-08-23T04:00:00.000Z' ,  '2023-09-05T04:00:00.000Z' , v_centerid2 , '1' , false , '#f6a700' ,  NOW() , 777 , true ) 
+		VALUES ( v_profid, v_calendarTimeStart2  , v_calendarTimeStart2 , v_specialty , '45' , '15' ,  'true' ,  'true'  ,  'true' ,  'true'  ,  'true'  , 'false'  ,  'false'  , v_calendarDateStart1 ,  v_calendarDateEnd2 , v_centerid2 , '1' , false , '#f6a700' ,  NOW() , 77777 , true ) 
 		RETURNING id INTO v_calendarid2 ; 
+	    -- INSERT INTO professional_calendar (professional_id , start_time,  end_time, specialty1, duration, time_between, monday, tuesday, wednesday, thursday, friday, saturday , sunday, date_start, date_end,   center_id,  status , deleted_professional, color ,date ,price , active ) VALUES ( '1',  '02:00:00-4' , '23:00:00-4', '100' , '50' , '10' ,  'true' ,  'true'  ,  'true' ,  'true'  ,  'true'  , 'false'  ,  'false'  ,   '2023-08-23T04:00:00.000Z'  ,  '2023-10-22T02:59:59.997Z'  ,   251 , '1' , false , '#FF4244' ,  '2023-08-23T21:51:21.307Z' ,  '666' , true ) ;
+		INSERT INTO professional_calendar (professional_id , start_time,  end_time, specialty1, duration, time_between, monday, tuesday, wednesday, thursday, friday, saturday , sunday, date_start, date_end,   center_id,  status , deleted_professional, color ,date ,price , active ) 
+		VALUES ( v_profid,  v_calendarTimeStart3 ,  v_calendarTimeStart3 , v_specialty , '45' , '15' ,  'true' ,  'true'  ,  'true' ,  'true'  ,  'true'  , 'false'  ,  'false'  , v_calendarDateStart1 ,  v_calendarDateEnd2 , v_centerid3 , '1' , false , '#f6a700' ,  NOW() , 88888 , true ) 
+		RETURNING id INTO v_calendarid3 ; 
 	    -- INSERT INTO professional_calendar (professional_id , start_time,  end_time, specialty1, duration, time_between, monday, tuesday, wednesday, thursday, friday, saturday , sunday, date_start, date_end,   center_id,  status , deleted_professional, color ,date ,price , active ) VALUES ( '1',  '02:00:00-4' , '23:00:00-4', '100' , '50' , '10' ,  'true' ,  'true'  ,  'true' ,  'true'  ,  'true'  , 'false'  ,  'false'  ,   '2023-08-23T04:00:00.000Z'  ,  '2023-10-22T02:59:59.997Z'  ,   251 , '1' , false , '#FF4244' ,  '2023-08-23T21:51:21.307Z' ,  '666' , true ) ;
 		
 		
 		
 		-- INSERT APPOINTMENTS
 		INSERT INTO appointment (  date , start_time,  duration,  center_id, confirmation_status, professional_id, patient_doc_id, patient_name,    patient_email, patient_phone1,  patient_age,  app_available, app_status, app_blocked, app_public,  location1, location2, location3, location4, location5, location6,   app_type_home, app_type_center,  app_type_remote, patient_notification_email_reserved , specialty_reserved , patient_address , calendar_id , app_price)   
-		VALUES ( '2023-09-15T11:00:00.000Z' , '2023-09-15T11:00:00.000Z' , '10' ,  v_centerid1 , '0' , v_profid , '13909371--2' ,  CONCAT('MATILDE AURORA MIRANDA',v_profname)  , 'ALEJANDRO2141@NADA.COM' , '555566534' ,  '43' ,'false' , '1' , '0' , '1', 1708 , 1662 ,null ,null ,null ,null , 'false' , 'true' , 'false' , '1' , '100' , 'null'  , v_calendarid1 , '666' 	) ; 
+		VALUES ( '2023-09-15T11:00:00.000Z' , '2023-09-15T11:00:00.000Z' , '10' ,  v_centerid1 , '0' , v_profid , '13909371--2' ,  CONCAT('MATILDE AURORA MIRANDA',v_profname)  , 'ALEJANDRO2141@NADA.COM' , '555566534' ,  '43' ,'false' , '1' , '0' , '1', 1708 , 1662 ,null ,null ,null ,null , 'false' , 'true' , 'false' , '1' , v_specialty , 'null'  , v_calendarid1 , '666' 	) ; 
 
 		INSERT INTO appointment (  date , start_time,  duration,  center_id, confirmation_status, professional_id, patient_doc_id, patient_name,    patient_email, patient_phone1,  patient_age,  app_available, app_status, app_blocked, app_public,  location1, location2, location3, location4, location5, location6,   app_type_home, app_type_center,  app_type_remote, patient_notification_email_reserved , specialty_reserved , patient_address , calendar_id , app_price)   
-		VALUES ('2023-09-15T14:00:00.000Z' , '2023-09-15T14:00:00.000Z' , '10' ,  v_centerid2 , '0' , v_profid , '13909371--2' ,  CONCAT('MATILDE AURORA MIRANDA',v_profname)  , 'ALEJANDRO2141@NADA.COM' , '555566534' ,  '43' ,'false' , '1' , '0' , '1', 1708 , 1662 ,null ,null ,null ,null , 'false' , 'true' , 'false' , '1' , '100' , 'null'  , v_calendarid2 , '666' 	) ; 
+		VALUES ('2023-09-15T14:00:00.000Z' , '2023-09-15T14:00:00.000Z' , '10' ,  v_centerid2 , '0' , v_profid , '13909371--2' ,  CONCAT('MATILDE AURORA MIRANDA',v_profname)  , 'ALEJANDRO2141@NADA.COM' , '555566534' ,  '43' ,'false' , '1' , '0' , '1', 1708 , 1662 ,null ,null ,null ,null , 'false' , 'true' , 'false' , '1' , v_specialty , 'null'  , v_calendarid2 , '666' 	) ; 
 
 		INSERT INTO appointment (  date , start_time,  duration,  center_id, confirmation_status, professional_id, patient_doc_id, patient_name,    patient_email, patient_phone1,  patient_age,  app_available, app_status, app_blocked, app_public,  location1, location2, location3, location4, location5, location6,   app_type_home, app_type_center,  app_type_remote, patient_notification_email_reserved , specialty_reserved , patient_address , calendar_id , app_price)   
-		VALUES ( NOW() , NOW() , '10' ,  v_centerid1 , '0' , v_profid , '13909371--2' ,  CONCAT('MATILDE AURORA MIRANDA',v_profname)  , 'ALEJANDRO2141@NADA.COM' , '555566534' ,  '43' ,'false' , '1' , '0' , '1', 1708 , 1662 ,null ,null ,null ,null , 'false' , 'true' , 'false' , '1' , '100' , 'null'  , v_calendarid1 , '666' 	) ; 
+		VALUES ( NOW() , NOW() , '10' ,  v_centerid1 , '0' , v_profid , '13909371-2' ,  CONCAT('MATILDE AURORA MIRANDA',v_profname)  , 'ALEJANDRO2141@NADA.COM' , '555566534' ,  '43' ,'false' , '1' , '0' , '1', 1708 , 1662 ,null ,null ,null ,null , 'false' , 'true' , 'false' , '1' , v_specialty , 'null'  , v_calendarid1 , '666' 	) ; 
 
 		INSERT INTO appointment (  date , start_time,  duration,  center_id, confirmation_status, professional_id, patient_doc_id, patient_name,    patient_email, patient_phone1,  patient_age,  app_available, app_status, app_blocked, app_public,  location1, location2, location3, location4, location5, location6,   app_type_home, app_type_center,  app_type_remote, patient_notification_email_reserved , specialty_reserved , patient_address , calendar_id , app_price)   
-		VALUES (NOW() , NOW(), '10' ,  v_centerid2 , '0' , v_profid , '13909371--2' ,  CONCAT('MATILDE AURORA MIRANDA',v_profname)  , 'ALEJANDRO2141@NADA.COM' , '555566534' ,  '43' ,'false' , '1' , '0' , '1', 1708 , 1662 ,null ,null ,null ,null , 'false' , 'true' , 'false' , '1' , '100' , 'null'  , v_calendarid2 , '666' 	) ; 
-
-
+		VALUES (NOW() , NOW(), '10' ,  v_centerid2 , '0' , v_profid , '13909371-2' ,  CONCAT('MATILDE AURORA MIRANDA',v_profname)  , 'ALEJANDRO2141@NADA.COM' , '555566534' ,  '43' ,'false' , '1' , '0' , '1', 1708 , 1662 ,null ,null ,null ,null , 'false' , 'true' , 'false' , '1' , v_specialty , 'null'  , v_calendarid2 , '666' 	) ; 
 
  	end loop;
+	
+end loop;
+
 
 
 end ; $$
-
-
+/*
+1507	"Huara"
+1508	"Huasco"
+1509	"Huechuraba"
+1510	"Illapel"
+1511	"Independencia"
+1512	"Iquique"
+1513	"Isla De Maipo"
+1514	"Isla De Pascua"
+1515	"Juan Fernandez"
+1516	"La Calera"
+1517	"La Cisterna"
 					
+*/
